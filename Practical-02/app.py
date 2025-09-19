51d581a2d787a2b86b21c94b660cd3b77ec67304
Sanjay1705 <sanjaymehra1715@gmail.com>
2025-09-19 14:55:59 +0000

    Update Jenkinsfiles

--- Practical-02/app.py
+++ Practical-02/app.py
@@ -13,6 +13,6 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5002))
     print(f"Starting Flask on port {port}")
     app.run(host="0.0.0.0", port=port)
