cd44bd614dd40c85f6997cd2786fbcc323205e1c
Sanjay1705 <sanjaymehra1715@gmail.com>
2025-09-19 15:38:39 +0000

    Update app.py and Dockerfile

--- Practical-02/app.py
+++ Practical-02/app.py
@@ -13,6 +13,4 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    print(f"Starting Flask on port {port}")
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=5000)
